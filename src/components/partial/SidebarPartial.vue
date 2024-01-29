c15a967af5fb63de3447d989a14c18a8e7933cb0
suharyadi <yadisuhar10@gmail.com>
2024-01-29 14:50:16 +0700

    - process base mata pelajaran

--- src/components/partial/SidebarPartial.vue
+++ src/components/partial/SidebarPartial.vue
@@ -33,6 +33,11 @@
             <i class="bi bi-circle"></i><span>Mata Pelajaran</span>
             </router-link>
         </li>
+        <li>
+            <router-link to="/base-mata-pelajaran" class="nav-link collapsed">
+            <i class="bi bi-circle"></i><span>Base Mata Pelajaran</span>
+            </router-link>
+        </li>
         </ul>
     </li>
 
